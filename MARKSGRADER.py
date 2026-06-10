3d8c48e9b3aef2fa2509077c7d932abbf3b36df0
garvitjani18-bit <garvitjani18@gmail.com>
2026-06-10 22:20:53 +0530

    Add files via upload

---
+++ MARKSGRADER.py
@@ -0,0 +1,9 @@
+marks=int(input("enter the marks:"))
+if(marks>90):
+    print("grade A")
+elif(marks>80):
+    print("grade B")
+elif(marks>70):
+    print("grade C")
+else:
+    print("YOUR STUDENT IS FAILED")
